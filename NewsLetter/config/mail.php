0341b0820676ed0f3fa0e67351ef2e1cdfc5ff80
Anas-Diabi <33428345+Hackermans007@users.noreply.github.com>
2024-02-08 11:18:00 +0100

    reset password fixed.

--- NewsLetter/config/mail.php
+++ NewsLetter/config/mail.php
@@ -109,7 +109,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'anas500.diabi@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('MAIL_FROM_NAME', 'Hello'),
     ],
 
     /*
